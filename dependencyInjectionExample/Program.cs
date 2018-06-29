d1a47989d96f8e9fdc218933eac32d2ef36703e9
Patrick Stockton <pstockton@plex.com>
2018-06-29 15:28:30 -0400

    Add project files.

---
+++ dependencyInjectionExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dependencyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
